e5807186d7291108aa1f63862a50052c27fcdf68
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-07-27 01:10:46 -0300

    carousel controls & auto-play

---
+++ js/carousel-control.js
@@ -0,0 +1,54 @@
+let slides = document.querySelector("#carousel-item-div").children;
+let totalSlides = document.querySelector("#carousel-item-div").children.length;
+let leftSlide = document.querySelector(".left-control");
+let rightSlide = document.querySelector(".right-control");
+let count = 0;
+
+leftSlide.onclick = function() {
+    moveSlide("prev");
+    resetTimer();
+}
+
+rightSlide.onclick = function() {
+    moveSlide("next");
+    resetTimer();
+}
+
+function moveSlide(direction) {
+
+    if(direction == "next") {
+        count++;
+        if(count == totalSlides) {
+            count = 0;
+        }
+    } else {
+        if(count == 0) {
+            count = totalSlides - 1;
+        } else {
+            count--;
+        }
+    }
+
+
+    for(let x = 0; x < slides.length; x++) {
+        slides[x].classList.remove("carousel-item-active");
+    }
+
+    slides[count].classList.add("carousel-item-active")
+
+}
+
+// AUTO-PLAY //
+
+var timer = setInterval(autoPlay, 6000);
+
+function autoPlay() {
+    moveSlide("next");
+}
+
+function resetTimer() {
+    // Limpa ao ser clicado para mover para frente ou tras
+    clearInterval(timer);
+    // Comeca novamente o timer
+    timer = setInterval(autoPlay, 6000);
+}
