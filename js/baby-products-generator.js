be57adbd7ee2dd941685ec490c395d511a111c57
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-07-30 15:05:18 -0300

    using generateHTML() inside generateProducts()

--- js/baby-products-generator.js
+++ js/baby-products-generator.js
@@ -10,21 +10,27 @@ requestJSON = () => {
     }
 }
 
+// Gera HTML do produto 
+generateHTML = (json, x) => {
+    productsContainer.innerHTML += `
+
+            <div class="single-product">
+                <img src="`+json[x].img+`">
+                <span class="catalog-line"></span>
+                <p class="product-title">`+json[x].name+`</p>
+                <p class="product-price">R$`+json[x].price+`,00</p>
+                <a class="product-button" key="`+json[x].id+`" href="#">Comprar</a>
+            </div>
+
+    `
+}
+
+// Percorre o JSON e gera
 generateProducts = () => {
     requestJSON();
     for(let x = 0; x < jsonObject.products.length; x++) {
         if(jsonObject.products[x].type =='baby') {
-            productsContainer.innerHTML += `
-        
-            <div class="single-product">
-                <img src="`+jsonObject.products[x].img+`">
-                <span class="catalog-line"></span>
-                <p class="product-title">`+jsonObject.products[x].name+`</p>
-                <p class="product-price">R$`+jsonObject.products[x].price+`,00</p>
-                <a class="product-button" key="`+jsonObject.products[x].id+`" href="#">Comprar</a>
-            </div>
-    
-            `;
+            generateHTML(jsonObject.products, x);
         }
     }
 }
@@ -51,22 +57,6 @@ selectedFilter = () => {
     } 
 
 }
-
-// Gera produto 
-generateHTML = (json, x) => {
-    productsContainer.innerHTML += `
-
-            <div class="single-product">
-                <img src="`+json[x].img+`">
-                <span class="catalog-line"></span>
-                <p class="product-title">`+json[x].name+`</p>
-                <p class="product-price">R$`+json[x].price+`,00</p>
-                <a class="product-button" key="`+json[x].id+`" href="#">Comprar</a>
-            </div>
-
-    `
-}
-
 
 
 // Filtro preco minimo e maximo 
